2c05e69b65ec6da939010b81d242480134030f95
Raniere Viana <ranieriviana@gmail.com>
2020-03-12 12:06:02 -0300

    Inicial

---
+++ CustomAuthenticationSB/src/main/java/br/com/sisinfor/CustomAuthenticationSbApplication.java
@@ -0,0 +1,13 @@
+package br.com.sisinfor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomAuthenticationSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomAuthenticationSbApplication.class, args);
+	}
+
+}
---
+++ CustomAuthenticationSB/src/test/java/br/com/sisinfor/CustomAuthenticationSbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sisinfor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomAuthenticationSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
